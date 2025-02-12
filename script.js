4210594096eab0bd038467c8ede0f8ef5404f1bf
gil-sousa2180 <gilvani.ss@hotmail.com>
2025-02-11 21:07:39 -0300

    Projeto finalizado com sucesso

---
+++ script.js
@@ -0,0 +1,63 @@
+const text = document.querySelector("#text")
+const upload = document.querySelector("#upload")
+const voice = document.querySelector("#voice")
+const listenBtn = document.querySelector("#listen-btn")
+const downlondBtn = document.querySelector("#downlond-btn")
+
+const speak = new SpeechSynthesisUtterance()
+
+let availableVoices = []
+
+const attValues = () => {
+    availableVoices = window.speechSynthesis.getVoices()
+
+    speak.voice = availableVoices[0]
+
+    console.log(availableVoices)
+
+    availableVoices.forEach((voices, index) => {
+        const option = document.createElement("option")
+        option.value = index
+        option.textContent = voices.name
+        voice.appendChild(option)
+    })   
+}
+
+window.speechSynthesis.onvoiceschanged = attValues
+
+voice.addEventListener("change", () => {
+    speak.voice = availableVoices[voice.value]
+})
+
+listenBtn.addEventListener("click", () => {
+    speak.text = text.value
+
+    window.speechSynthesis.speak(speak)
+})
+
+downlondBtn.addEventListener("click", () => {
+    const downText = text.value
+
+    const blob = new Blob([downText], { type: "text/plain"})
+
+    const url = URL.createObjectURL(blob)
+
+    const a = document.createElement("a")
+
+    a.href = url
+    a.download = "conversor.txt"
+    a.click()
+
+    URL.revokeObjectURL(url)
+})
+
+upload.addEventListener("change", (event) => {
+    const archive = event.target.files[0]
+    if (archive) {
+        const reader = new FileReader()
+        reader.onload = (e) => {
+            text.value = e.target.result
+        }
+            reader.readAsText(archive)
+    }
+})
